9d579f0263d62ca9667287c5ab4646e09389ab8e
Andreas Krapalies <akrapalies@gmail.com>
2023-09-10 18:34:52 +0200

    change of upate_board.js due to png location

--- js/update_board.js
+++ js/update_board.js
@@ -1,5 +1,29 @@
+// Global Elements
+let todos = [
+    {
+        'id':0,
+        'title':'ToDo Test Titel',
+        'category':'todo'
+    },
+    {
+        'id':1,
+        'title':'In Progress Test Titel',
+        'category':'inprogress'
+    },
+    {
+        'id':2,
+        'title':'Await Feedback Test Titel',
+        'category':'awaitfeedback'
+    },
+    {
+        'id':3,
+        'title':'Done Test Titel',
+        'category':'done'
+    }
+];
+
+let currentDraggedElement;
 function renderBoard(){
-    alert('Test');
     let board = document.getElementById('dashboard-content');
     board.innerHTML = /*html*/`
         <div class="board-header">
@@ -10,13 +34,13 @@ function renderBoard(){
             <form class="find-task">
                 <div class="search-field">
                     <input id="searchBarInput" type="text" placeholder="Find Task">
-                    <img src="png/verticalLine.png">
-                    <img class="magnifier" src="png/search.png">
+                    <img src="./../img/verticalLine.png">
+                    <img class="magnifier" src="./../img/search.png">
                 </div>
 
                 <button class="addTaskButton">
                     <p>Add task</p>
-                    <img src="png/add.png">
+                    <img src="./../img/add.png">
                 </button>
             </form>
         </div>
@@ -25,7 +49,7 @@ function renderBoard(){
         <div class="board-card">
             <div class="board-card-header">
                 <p>To do</p>
-                <img src="png/Capa.png">
+                <img src="./../img/Capa.png">
             </div>
             <div class="drag-area" id="boardCardToDo" ondrop="moveTo('todo')" ondragleave="removeHighlight('open')" ondragover="allowDrop(event); highlight('open')">
 
@@ -34,7 +58,7 @@ function renderBoard(){
         <div class="board-card">
             <div class="board-card-header">
                 <p>In progress</p>
-                <img src="png/Capa.png">
+                <img src="./../img/Capa.png">
             </div>
             <div  class="drag-area" id="boardCardInProgress" ondrop="moveTo('inprogress')" ondragleave="removeHighlight('open')" ondragover="allowDrop(event); highlight('open')">
 
@@ -43,7 +67,7 @@ function renderBoard(){
         <div class="board-card">
             <div class="board-card-header">
                 <p>Await feedback</p>
-                <img src="png/Capa.png">
+                <img src="./../img/Capa.png">
             </div>
             <div class="drag-area" id="boardCardAwaitFeedback" ondrop="moveTo('awaitfeedback')" ondragleave="removeHighlight('open')" ondragover="allowDrop(event); highlight('open')">
 
@@ -52,7 +76,7 @@ function renderBoard(){
         <div class="board-card">
             <div class="board-card-header">
                 <p>Done</p>
-                <img src="png/Capa.png">
+                <img src="./../img/Capa.png">
             </div>
             <div class="drag-area" id="boardCardDone" ondrop="moveTo('done')" ondragleave="removeHighlight('open')" ondragover="allowDrop(event); highlight('open')">
 
@@ -60,4 +84,60 @@ function renderBoard(){
         </div>
     </div>
     `
+    updateBoard();
+}
+
+function updateBoard(){
+    let todo = todos.filter(t => t['category'] == 'todo');
+    document.getElementById('boardCardToDo').innerHTML = '';
+
+    todo.forEach(element => {
+        document.getElementById('boardCardToDo').innerHTML += generateToDoHTML(element);
+    });
+    
+    let inprogress = todos.filter(i => i['category'] == 'inprogress');
+    document.getElementById('boardCardInProgress').innerHTML = '';
+
+    inprogress.forEach(element => {
+        document.getElementById('boardCardInProgress').innerHTML += generateToDoHTML(element);
+    });
+
+    let awaitfeedback = todos.filter(a => a['category'] == 'awaitfeedback');
+    document.getElementById('boardCardAwaitFeedback').innerHTML = '';
+
+    awaitfeedback.forEach(element => {
+        document.getElementById('boardCardAwaitFeedback').innerHTML += generateToDoHTML(element);
+    });
+
+    let done = todos.filter(d => d['category'] == 'done');
+    document.getElementById('boardCardDone').innerHTML = '';
+
+    done.forEach(element =>{
+        document.getElementById('boardCardDone').innerHTML += generateToDoHTML(element);
+    });
+}
+
+function generateToDoHTML(element){
+    return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="card">${element['title']}</div>`;
+}
+
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function moveTo(category) {
+    todos[currentDraggedElement]['category'] = category;
+    updateBoard();
+}
+
+function highlight(id) {
+    document.getElementById(id).classList.add('drag-area-highlight');
+}
+
+function removeHighlight(id) {
+    document.getElementById(id).classList.remove('drag-area-highlight');
 }
