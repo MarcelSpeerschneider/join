be20bd129063c776f3a88c4134514b0b8e4814a3
Ivan <ivanvilargomes@gmail.com>
2023-09-13 12:45:51 +0200

    contact logic js

--- js/contacts.js
+++ js/contacts.js
@@ -1,54 +1,122 @@
-let contacts = [];
+let contacts = [
+  {
+    name: "Peter Müller",
+    email: "peter@example.com",
+    phone: "123-456-7890",
+    color: "#FF5733",
+  },
+  {
+    name: "Susanne Schmidt",
+    email: "susanne@example.com",
+    phone: "555-555-5555",
+    color: "#33FF57",
+  },
+  {
+    name: "Michaela Meier",
+    email: "michaela@example.com",
+    phone: "987-654-3210",
+    color: "#FF3366",
+  },
+  {
+    name: "Andreas Wagner",
+    email: "andreas@example.com",
+    phone: "555-123-4567",
+    color: "#3399FF",
+  },
+  {
+    name: "Janine Becker",
+    email: "janine@example.com",
+    phone: "789-456-1230",
+    color: "#33FF99",
+  },
+  {
+    name: "Matthias Huber",
+    email: "matthias@example.com",
+    phone: "111-222-3333",
+    color: "#FF9933",
+  },
+  {
+    name: "Christina Schneider",
+    email: "christina@example.com",
+    phone: "444-555-444",
+    color: "#9966FF",
+  },
+  {
+    name: "Stefan Fischer",
+    email: "stefan@example.com",
+    phone: "777-888-9999",
+    color: "#66FF33",
+  },
+  {
+    name: "Sabine Hoffmann",
+    email: "sabine@example.com",
+    phone: "222-333-4444",
+    color: "#FF6633",
+  },
+  {
+    name: "Markus Schuster",
+    email: "markus@example.com",
+    phone: "666-777-8888",
+    color: "#33FFCC",
+  },
+];
 
 function renderContacts() {
-    let content = document.getElementById("dashboard-content");
-    content.innerHTML = "";
-    content.innerHTML += returnRenderHTML();
-  
-    let contactListElement = document.getElementById("firstLetterContainer");
-    contactListElement.innerHTML = "";
-  
-    contacts.sort((a, b) => a.name.localeCompare(b.name));
-  
-    let currentLetter = null;
-  
-    for (let contact of contacts) {
-      let firstLetter = contact.name.charAt(0).toUpperCase();
-  
-      if (firstLetter !== currentLetter) {
-        currentLetter = firstLetter;
-  
-        contactListElement.innerHTML += `
-              <span class="current-letter-content">${currentLetter}</span>
-            <div class="first-letter-of-name" id="contacts-${currentLetter.toLowerCase()}"></div>
-        `;
-      }
-  
-      let letterContactsElement = document.getElementById(`contacts-${currentLetter.toLowerCase()}`);
-  
-      letterContactsElement.innerHTML += `
-      <div class="list-of-contacts">
-        <div class="contact-profil-img" >
-        <span style="background-color:${contact.color}" class="profil-icon">${contact.name.charAt(0)}</span>
-        </div>
-        <div class="name-email">
-          <div class="name">
-            <span>${contact.name}</span>
-          </div>
-          <div class="email">
-            ${contact.email}
-          </div>
-          <div class="number">${contact.phone}</div>
-        </div>
-        </div>
-      `;
+  let content = document.getElementById("dashboard-content");
+  content.innerHTML = "";
+  content.innerHTML += returnRenderHTML();
+
+  let contactListElement = document.getElementById("firstLetterContainer");
+  contactListElement.innerHTML = "";
+
+  contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+  let currentLetter = null;
+
+  for (let contact of contacts) {
+    let firstLetter = contact.name.charAt(0).toUpperCase();
+
+    if (firstLetter !== currentLetter) {
+      currentLetter = firstLetter;
+      contactListElement.innerHTML += generateLetterContainers(currentLetter);
     }
+
+    let letterContactsElement = document.getElementById(
+      `contacts-${currentLetter.toLowerCase()}`
+    );
+
+    letterContactsElement.innerHTML += generateContactHTML(contact);
   }
-  
+}
 
+function generateLetterContainers(currentLetter) {
+  return /*html*/ `
+    <span class="current-letter-content">${currentLetter}</span>
+    <div class="first-letter-of-name" id="contacts-${currentLetter.toLowerCase()}"></div>
+  `;
+}
 
-function returnRenderHTML(){
-    return /*html*/ `
+function generateContactHTML(contact) {
+  return /*html*/ `
+    <div class="list-of-contacts">
+      <div class="contact-profil-img">
+        <span style="background-color:${
+          contact.color
+        }" class="profil-icon">${contact.name.charAt(0)}</span>
+      </div>
+      <div class="name-email">
+        <div class="name">
+          <span>${contact.name}</span>
+        </div>
+        <div class="email">${contact.email}</div>
+        <div class="number">${contact.phone}</div>
+      </div>
+    </div>
+  `;
+}
+
+function returnRenderHTML() {
+  return /*html*/ `
     <div class="contact-container">
         <div class="contact-content">
         <div class="contact-list">
@@ -69,7 +137,15 @@ function returnRenderHTML(){
     <div class="showContactRight">
         <div class="contacts-header">
             <h1>Contacts</h1>
+            <div>
+            <svg width="4" height="63" viewBox="0 0 4 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path id="Vector 5" d="M2 2V61" stroke="#29ABE2" stroke-width="3" stroke-linecap="round"/>
+            </svg>
             </div>
+            <span>Better with a Team</span>
+            </div>
+
+            <div id="contactsShow" class="contact-onlick-content"></div>
         </div>
     <div style="display:none;" id="contactPopUp" class="contact-pop-up">
             <div class="pup-up-content" action="">
@@ -90,7 +166,7 @@ function returnRenderHTML(){
      </div>
         <h1>Add Contact</h1>
         <span>Tasks are better with a team!</span>
-        <div class="blue-line"></div>
+        <div class="blue-line"> </div>
     </div>
     <div class="pop-up-right">
         <div onclick="closeWindow()" class="closeBtn">
@@ -116,16 +192,63 @@ function returnRenderHTML(){
             <form onsubmit="addNewContact(event);return false;">
                 <div class="input-img">
                 <input required  placeholder="Name" id="name" type="text">
+                <svg class="svgStyleInput" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="person">
+                <mask id="mask0_84485_2192" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                <rect id="Bounding box" width="24" height="24" fill="#D9D9D9"/>
+                </mask>
+                <g mask="url(#mask0_84485_2192)">
+                <path id="person_2" d="M12 12C10.9 12 9.95833 11.6083 9.175 10.825C8.39167 10.0417 8 9.1 8 8C8 6.9 8.39167 5.95833 9.175 5.175C9.95833 4.39167 10.9 4 12 4C13.1 4 14.0417 4.39167 14.825 5.175C15.6083 5.95833 16 6.9 16 8C16 9.1 15.6083 10.0417 14.825 10.825C14.0417 11.6083 13.1 12 12 12ZM18 20H6C5.45 20 4.97917 19.8042 4.5875 19.4125C4.19583 19.0208 4 18.55 4 18V17.2C4 16.6333 4.14583 16.1125 4.4375 15.6375C4.72917 15.1625 5.11667 14.8 5.6 14.55C6.63333 14.0333 7.68333 13.6458 8.75 13.3875C9.81667 13.1292 10.9 13 12 13C13.1 13 14.1833 13.1292 15.25 13.3875C16.3167 13.6458 17.3667 14.0333 18.4 14.55C18.8833 14.8 19.2708 15.1625 19.5625 15.6375C19.8542 16.1125 20 16.6333 20 17.2V18C20 18.55 19.8042 19.0208 19.4125 19.4125C19.0208 19.8042 18.55 20 18 20ZM6 18H18V17.2C18 17.0167 17.9542 16.85 17.8625 16.7C17.7708 16.55 17.65 16.4333 17.5 16.35C16.6 15.9 15.6917 15.5625 14.775 15.3375C13.8583 15.1125 12.9333 15 12 15C11.0667 15 10.1417 15.1125 9.225 15.3375C8.30833 15.5625 7.4 15.9 6.5 16.35C6.35 16.4333 6.22917 16.55 6.1375 16.7C6.04583 16.85 6 17.0167 6 17.2V18ZM12 10C12.55 10 13.0208 9.80417 13.4125 9.4125C13.8042 9.02083 14 8.55 14 8C14 7.45 13.8042 6.97917 13.4125 6.5875C13.0208 6.19583 12.55 6 12 6C11.45 6 10.9792 6.19583 10.5875 6.5875C10.1958 6.97917 10 7.45 10 8C10 8.55 10.1958 9.02083 10.5875 9.4125C10.9792 9.80417 11.45 10 12 10Z" fill="#A8A8A8"/>
+                </g>
+                </g>
+                </svg>
                 </div>
-                <div>
+                <div class="input-img">
                 <input required class="contactMail" placeholder="Email" id="email" type="email">
+                <svg class="svgStyleInput" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="mail">
+<mask id="mask0_84485_2199" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+<rect id="Bounding box" width="24" height="24" fill="#D9D9D9"/>
+</mask>
+<g mask="url(#mask0_84485_2199)">
+<path id="mail_2" d="M4 20C3.45 20 2.97917 19.8042 2.5875 19.4125C2.19583 19.0208 2 18.55 2 18V6C2 5.45 2.19583 4.97917 2.5875 4.5875C2.97917 4.19583 3.45 4 4 4H20C20.55 4 21.0208 4.19583 21.4125 4.5875C21.8042 4.97917 22 5.45 22 6V18C22 18.55 21.8042 19.0208 21.4125 19.4125C21.0208 19.8042 20.55 20 20 20H4ZM20 8L12.525 12.675C12.4417 12.725 12.3542 12.7625 12.2625 12.7875C12.1708 12.8125 12.0833 12.825 12 12.825C11.9167 12.825 11.8292 12.8125 11.7375 12.7875C11.6458 12.7625 11.5583 12.725 11.475 12.675L4 8V18H20V8ZM12 11L20 6H4L12 11ZM4 8.25V6.775V6.8V6.7875V8.25Z" fill="#A8A8A8"/>
+</g>
+</g>
+</svg>
                 </div>
-                <input required placeholder="Phone number" id="number" type="number">
-
+                <div class="input-img">
+                <input required placeholder="Phone number" onwheel="this.blur();"  id="number" type="number">
+                <svg class="svgStyleInput" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="call">
+<mask id="mask0_84485_2206" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
+<rect id="Bounding box" y="0.5" width="24" height="24" fill="#D9D9D9"/>
+</mask>
+<g mask="url(#mask0_84485_2206)">
+<path id="call_2" d="M19.95 21.5C17.8667 21.5 15.8083 21.0458 13.775 20.1375C11.7417 19.2292 9.89167 17.9417 8.225 16.275C6.55833 14.6083 5.27083 12.7583 4.3625 10.725C3.45417 8.69167 3 6.63333 3 4.55C3 4.25 3.1 4 3.3 3.8C3.5 3.6 3.75 3.5 4.05 3.5H8.1C8.33333 3.5 8.54167 3.57917 8.725 3.7375C8.90833 3.89583 9.01667 4.08333 9.05 4.3L9.7 7.8C9.73333 8.06667 9.725 8.29167 9.675 8.475C9.625 8.65833 9.53333 8.81667 9.4 8.95L6.975 11.4C7.30833 12.0167 7.70417 12.6125 8.1625 13.1875C8.62083 13.7625 9.125 14.3167 9.675 14.85C10.1917 15.3667 10.7333 15.8458 11.3 16.2875C11.8667 16.7292 12.4667 17.1333 13.1 17.5L15.45 15.15C15.6 15 15.7958 14.8875 16.0375 14.8125C16.2792 14.7375 16.5167 14.7167 16.75 14.75L20.2 15.45C20.4333 15.5167 20.625 15.6375 20.775 15.8125C20.925 15.9875 21 16.1833 21 16.4V20.45C21 20.75 20.9 21 20.7 21.2C20.5 21.4 20.25 21.5 19.95 21.5ZM6.025 9.5L7.675 7.85L7.25 5.5H5.025C5.10833 6.18333 5.225 6.85833 5.375 7.525C5.525 8.19167 5.74167 8.85 6.025 9.5ZM14.975 18.45C15.625 18.7333 16.2875 18.9583 16.9625 19.125C17.6375 19.2917 18.3167 19.4 19 19.45V17.25L16.65 16.775L14.975 18.45Z" fill="#A8A8A8"/>
+</g>
+</g>
+</svg>
+                </div>
                 <div class="contactBtn">
-                <button class="cancelBtn">Cancel</button>
+                <button class="cancelBtn">
+                  Cancel
+                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g id="iconoir:cancel">
+                  <path id="Vector" d="M12.001 12.5001L17.244 17.7431M6.758 17.7431L12.001 12.5001L6.758 17.7431ZM17.244 7.25708L12 12.5001L17.244 7.25708ZM12 12.5001L6.758 7.25708L12 12.5001Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </g>
+                  </svg>  
+                </button>
                 <button class="createBtn">Create Contact
-                    <img src="assets/img/close.svg" alt="">
+                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="check">
+                <mask id="mask0_84485_3902" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
+                <rect id="Bounding box" y="0.5" width="24" height="24" fill="#D9D9D9"/>
+                </mask>
+                <g mask="url(#mask0_84485_3902)">
+                <path id="check_2" d="M9.55057 15.65L18.0256 7.175C18.2256 6.975 18.4631 6.875 18.7381 6.875C19.0131 6.875 19.2506 6.975 19.4506 7.175C19.6506 7.375 19.7506 7.6125 19.7506 7.8875C19.7506 8.1625 19.6506 8.4 19.4506 8.6L10.2506 17.8C10.0506 18 9.81724 18.1 9.55057 18.1C9.28391 18.1 9.05057 18 8.85057 17.8L4.55057 13.5C4.35057 13.3 4.25474 13.0625 4.26307 12.7875C4.27141 12.5125 4.37557 12.275 4.57557 12.075C4.77557 11.875 5.01307 11.775 5.28807 11.775C5.56307 11.775 5.80057 11.875 6.00057 12.075L9.55057 15.65Z" fill="white"/>
+                </g>
+                </g>
+                </svg>
                 </button>
                 </div>
                 <div class="message" id="errorMessage"></div>
@@ -142,34 +265,34 @@ function returnRenderHTML(){
 }
 
 function addNewContact(event) {
-    event.preventDefault();
-  
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("number").value;
+  event.preventDefault();
 
-    let existingContact = contacts.find(contact => contact.email === email)
-    if(existingContact) {
-        let errorMessage = document.getElementById("errorMessage");
-        errorMessage.textContent = "A contact with the same email already exists!";
-        errorMessage.style.color = "red";
-        return;
-    }
-  
-    let newContact = {
-      name: name,
-      email: email,
-      phone: phone,
-    };
-  
-    contacts.push(newContact);
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("number").value;
 
-    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    newContact.color = randomColor;
-  
-    renderContacts();
-    closeWindow();
+  let existingContact = contacts.find((contact) => contact.email === email);
+  if (existingContact) {
+    let errorMessage = document.getElementById("errorMessage");
+    errorMessage.textContent = "A contact with the same email already exists!";
+    errorMessage.style.color = "red";
+    return;
   }
+
+  let newContact = {
+    name: name,
+    email: email,
+    phone: phone,
+  };
+
+  contacts.push(newContact);
+
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  newContact.color = randomColor;
+
+  renderContacts();
+  closeWindow();
+}
 
 function openContactForm() {
   document.getElementById("contactPopUp").style.display = "flex";
@@ -178,5 +301,3 @@ function openContactForm() {
 function closeWindow() {
   document.getElementById("contactPopUp").style.display = "none";
 }
-
-
