e9f2f5e9ea7890b2c0c6f0367d22222379b123bb
Ivan <ivanvilargomes@gmail.com>
2023-09-14 23:22:47 +0200

    contact js work delete contact

--- js/contacts.js
+++ js/contacts.js
@@ -75,29 +75,52 @@ function renderContacts() {
 
     const colors = ["#FF5733", "#33FF57", "#5733FF", "#FF33F9", "#33F9FF"];
 
+    let currentLetter=null;
+
     for (let i = 0; i < contacts.length; i++) {
         const contact = contacts[i];
-        const firstLetterOfFirstName = contact.name.split(" ")[0][0];
-        const firstLetterOfLastName = contact.name.split(" ")[1][0];
+        const firstLetterOfFirstName = contact.name.split(" ")[0][0].toLocaleUpperCase();
+        if (firstLetterOfFirstName !== currentLetter) {
+            currentLetter = firstLetterOfFirstName;
+            contactListElement.innerHTML += generateLetterContainer(currentLetter);
+        }
         const color = colors[i % colors.length];
-        contactListElement.innerHTML += /*html*/ `
-  <div onclick="displayContactInfo(${i}, '${color}')"  class="list-of-contacts">
-   <div class="contact-profil-img">
-   <span class="profil-icon"  style="background-color: ${color};" id="firstLetterOfContact">
-   ${firstLetterOfFirstName}${firstLetterOfLastName}
-  </span>
-   </div>
-   <div class="name-email">
-     <div class="name">
-       <span class="contact-name">${contact.name}</span>
-     </div>
-     <div class="email ">${contact.email}</div>
-     <div class="number">${contact.phone}</div>
-   </div>
- </div>
- `;
+        const contactHTML = generateContactHTML(contact, i, color);
+        contactListElement.innerHTML += contactHTML;
     }
 }
+
+function generateLetterContainer(currentLetter) {
+    return `
+        <div class="letter-container">
+            <span style="font-size:27px;border-bottom:2px solid lightgray;display:flex;padding:10px;">${currentLetter}</span>
+        </div>
+    `;
+}
+
+
+function generateContactHTML(contact, i, color) {
+    const firstLetterOfFirstName = contact.name.split(" ")[0][0].toUpperCase();
+    const firstLetterOfLastName = contact.name.split(" ")[1][0];
+
+    return /*html*/ `
+        <div onclick="displayContactInfo(${i}, '${color}')"  class="list-of-contacts">
+            <div class="contact-profil-img">
+                <span class="profil-icon"  style="background-color: ${color};" id="firstLetterOfContact">
+                    ${firstLetterOfFirstName}${firstLetterOfLastName}
+                </span>
+            </div>
+            <div class="name-email">
+                <div class="name">
+                    <span class="contact-name">${contact.name}</span>
+                </div>
+                <div class="email ">${contact.email}</div>
+                <div class="number">${contact.phone}</div>
+            </div>
+        </div>
+    `;
+}
+
 
 function sortContactsAlphabetically() {
     contacts.sort((a, b) => a.name.localeCompare(b.name));
@@ -111,27 +134,59 @@ function deleteContact(index) {
 function displayContactInfo(index, color) {
     const contact = contacts[index];
     let contentshow = document.getElementById("contactsShow");
-    contentshow.innerHTML = generateContactInfoHTML(contact, color);
+    contentshow.innerHTML = generateContactInfoHTML(contact,index, color);
 }
 
-function generateContactInfoHTML(contact, color) {
+function generateContactInfoHTML(contact,index, color) {
     const firstLetterOfFirstName = contact.name.split(' ')[0][0];
     const firstLetterOfLastName = contact.name.split(' ')[1][0];
     return /*html*/ `
     <div class="contact-info">
         <div class="info-name">
-          <div style="background-color: ${color};" class="profil-icon">${firstLetterOfFirstName}${firstLetterOfLastName}</div>
-            <span>${contact.name}</span>
+          <div style="background-color: ${color};" class="profil-icon-right">${firstLetterOfFirstName}${firstLetterOfLastName}</div>
+          <div class="edit-delete-name">
+            <span style="font-size:40px;margin-left:30px;margin-top:30px;">${contact.name}</span>
+            <div class="edit-delete">
+            <div id="edit-contact"  class="edit-delete-Hover">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="edit">
+<mask id="mask0_84485_4268" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+<rect id="Bounding box" width="24" height="24" fill="#D9D9D9"/>
+</mask>
+<g mask="url(#mask0_84485_4268)">
+<path id="edit_2" d="M5 19H6.4L15.025 10.375L13.625 8.975L5 17.6V19ZM19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3042 2.75 17.8625 2.75C18.4208 2.75 18.8917 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.57083 21.275 6.1125C21.2917 6.65417 21.1083 7.11667 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" fill="#2A3647"/>
+</g>
+</g>
+</svg>
+                Edit
+            </div>
+            <div onclick="deleteContact(${index})" id="deleteContactDiv" class="edit-delete-Hover">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="delete">
+<mask id="mask0_84485_4113" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+<rect id="Bounding box" width="24" height="24" fill="#D9D9D9"/>
+</mask>
+<g mask="url(#mask0_84485_4113)">
+<path id="delete_2" d="M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6C4.71667 6 4.47917 5.90417 4.2875 5.7125C4.09583 5.52083 4 5.28333 4 5C4 4.71667 4.09583 4.47917 4.2875 4.2875C4.47917 4.09583 4.71667 4 5 4H9C9 3.71667 9.09583 3.47917 9.2875 3.2875C9.47917 3.09583 9.71667 3 10 3H14C14.2833 3 14.5208 3.09583 14.7125 3.2875C14.9042 3.47917 15 3.71667 15 4H19C19.2833 4 19.5208 4.09583 19.7125 4.2875C19.9042 4.47917 20 4.71667 20 5C20 5.28333 19.9042 5.52083 19.7125 5.7125C19.5208 5.90417 19.2833 6 19 6V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM7 6V19H17V6H7ZM9 16C9 16.2833 9.09583 16.5208 9.2875 16.7125C9.47917 16.9042 9.71667 17 10 17C10.2833 17 10.5208 16.9042 10.7125 16.7125C10.9042 16.5208 11 16.2833 11 16V9C11 8.71667 10.9042 8.47917 10.7125 8.2875C10.5208 8.09583 10.2833 8 10 8C9.71667 8 9.47917 8.09583 9.2875 8.2875C9.09583 8.47917 9 8.71667 9 9V16ZM13 16C13 16.2833 13.0958 16.5208 13.2875 16.7125C13.4792 16.9042 13.7167 17 14 17C14.2833 17 14.5208 16.9042 14.7125 16.7125C14.9042 16.5208 15 16.2833 15 16V9C15 8.71667 14.9042 8.47917 14.7125 8.2875C14.5208 8.09583 14.2833 8 14 8C13.7167 8 13.4792 8.09583 13.2875 8.2875C13.0958 8.47917 13 8.71667 13 9V16Z" fill="#2A3647"/>
+</g>
+</g>
+</svg>
+                Delete
+            </div>
+            </div>
+            </div>
         </div>
-        <div>Contact Information</div>
-        <div class="info">
-            <label>Email:</label>
+        <div style="font-size:23px;margin-top:50px;">Contact Information</div>
+            <a class="info" href="mailto:${contact.email}">
+            <label>Email</label>
             <span>${contact.email}</span>
-        </div>
-        <div class="info">
-            <label>Phone:</label>
+            </a>
+
+            <a class="info" href="tel:${contact.phone}">
+            <label>Phone</label>
             <span>${contact.phone}</span>
-        </div>
+            </a>
+        
     </div>
 `;
 }
