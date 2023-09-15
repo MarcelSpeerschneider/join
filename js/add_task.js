58269d423934f3f0226ccc0b1fc860f1b8a2db77
Andreas Krapalies <akrapalies@gmail.com>
2023-09-15 08:06:24 +0200

    Worked add Task Overlay

--- js/add_task.js
+++ js/add_task.js
@@ -13,47 +13,81 @@ function renderPopUpAddTask() {
 
 function HTMLrenderPopUpAddTask() {
     return /*html*/`
-        <form id="addTaskOverlay">
-            <div id="addTaskOverlayHead">
-                <h1>Add Task</h1>
-                <img class="cross-close" onclick="closeAddTaskForm()" src="./../img/cross.png">
-            </div>
-            <div id="addTaskOverlayBody">
-                <div id="addTaskOverlayBodyLeft">
+    <form id="addTaskOverlay">
+        <div id="addTaskOverlayHead">
+            <h1>Add Task</h1>
+            <img class="cross-close" onclick="closeAddTaskForm()" src="./../img/cross.png">
+        </div>
+        <div id="addTaskOverlayBody">
+            <div id="addTaskOverlayBodyLeft">
+                <div id="elementInAddTaskOverlayBody">
                     <label>Title</label>
                     <input type="text" placeholder="Enter a title" required>
+                </div>
+                <div id="elementInAddTaskOverlayBody">
                     <label>Description</label>
                     <textarea cols="30" rows="10" placeholder="Enter a Description" required></textarea>
-                    <label for="categories" >Category</label>
+                </div>
+                <div id="elementInAddTaskOverlayBody">
+                    <label for="categories">Category</label>
                     <select name="categories" id="taskCategories">
                         <option value="" disabled selected>Select task category</option>
                     </select>
+                </div>
+                <div id="elementInAddTaskOverlayBody">
                     <label for="assignment">Assigned to</label>
                     <select name="assignment" id="taskAssignment" required>
                         <option value="" disabled selected>Select contacts to assign</option>
                     </select>
                 </div>
-                <div id="addTaskOverlayBodyMiddle"></div>
-                <div id="addTaskOverlayBodyRight">
+            </div>
+            <div id="addTaskOverlayBodyMiddle"></div>
+            <div id="addTaskOverlayBodyRight">
+                <div id="elementInAddTaskOverlayBody">
                     <label>Due Date</label>
                     <input type="date" placeholder="dd/mm/yyyy" required>
+                </div>
+                <div id="elementInAddTaskOverlayBody">
                     <label>Prio</label>
                     <div class="addtask-prio-main-container">
-                        <div class="addtask-prio-container" id="prio-urgent" onclick="prioContainer('urgent')" data-selected="true" style="background-color: rgb(59, 78, 105); color: rgb(255, 255, 255);">
+                        <div class="addtask-prio-container" id="prio-urgent" onclick="prioContainer('urgent')"
+                            data-selected="true" style="background-color: rgb(59, 78, 105); color: rgb(255, 255, 255);">
                             Urgent <img src="./../img/prio-urgent.svg">
                         </div>
-                        <div class="addtask-prio-container" id="prio-medium" onclick="prioContainer('medium')" data-selected="false" style="background-color: rgb(255, 255, 255); color: black;">
+                        <div class="addtask-prio-container" id="prio-medium" onclick="prioContainer('medium')"
+                            data-selected="false" style="background-color: rgb(255, 255, 255); color: black;">
                             Medium <img src="./../img/prio-medium.svg">
                         </div>
-                        <div class="addtask-prio-container" id="prio-low" onclick="prioContainer('low')" data-selected="false" style="background-color: rgb(255, 255, 255); color: black;">
+                        <div class="addtask-prio-container" id="prio-low" onclick="prioContainer('low')"
+                            data-selected="false" style="background-color: rgb(255, 255, 255); color: black;">
                             Low <img src="./../img/prio-low.svg">
                         </div>
                     </div>
-                        <label>Subtasks</label>
-                        <input>
-                        <button>Create</button>
+                </div>
+                <div id="elementInAddTaskOverlayBody">
+                    <label>Subtasks</label>
+                    <div class="subTask">
+                        <input class="test" type="text" placeholder="Add new subtask">
+                        <div class="subTaskImage">
+                            <img src="./../img/add-subtask.svg">
+                        </div>
+                    </div>
+                </div>
             </div>
-        </form>     
-    `
-        ;
+        </div>
+        <div class="addTaskOverlayBottom">
+            <div class="add-task-bottom-button-container">
+                <button class="button-clear" onmouseover="buttonCreateTaskChangeColor()"
+                    onmouseout="buttonCreateTaskChangeColorBack()">Clear<svg width="25" height="24" viewBox="0 0 25 24"
+                        fill="none" xmlns="http://www.w3.org/2000/svg" id="add-task-icon-cancel">
+                        <path
+                            d="M12.2495 12.0001L17.4925 17.2431M7.00653 17.2431L12.2495 12.0001L7.00653 17.2431ZM17.4925 6.75708L12.2485 12.0001L17.4925 6.75708ZM12.2485 12.0001L7.00653 6.75708L12.2485 12.0001Z"
+                            stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                </button>
+                <button class="button-create-task">Create Task<img src="./../img/check.svg"
+                        id="button-create-task"></button>
+            </div>
+    </form>
+    `;
 }
