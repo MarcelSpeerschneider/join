2b15a525a406bc6f1e6bcb3905bf13b41f90562e
Andreas Krapalies <akrapalies@gmail.com>
2023-09-16 13:36:51 +0200

    Worked on add_task.js

--- js/add_task.js
+++ js/add_task.js
@@ -20,21 +20,21 @@ function HTMLrenderPopUpAddTask() {
         </div>
         <div id="addTaskOverlayBody">
             <div id="addTaskOverlayBodyLeft">
-                <div id="elementInAddTaskOverlayBody">
+                <div class="elementInAddTaskOverlayBody">
                     <label>Title</label>
-                    <input type="text" placeholder="Enter a title" required>
+                    <input id="taskInputTitle" class="taskInput" type="text" placeholder="Enter a title" required>
                 </div>
-                <div id="elementInAddTaskOverlayBody">
+                <div class="elementInAddTaskOverlayBody">
                     <label>Description</label>
-                    <textarea cols="30" rows="10" placeholder="Enter a Description" required></textarea>
+                    <textarea id="taskInputDescription" class="taskInput" cols="30" rows="10" placeholder="Enter a Description" required></textarea>
                 </div>
-                <div id="elementInAddTaskOverlayBody">
+                <div class="elementInAddTaskOverlayBody">
                     <label for="categories">Category</label>
                     <select name="categories" id="taskCategories">
                         <option value="" disabled selected>Select task category</option>
                     </select>
                 </div>
-                <div id="elementInAddTaskOverlayBody">
+                <div class="elementInAddTaskOverlayBody">
                     <label for="assignment">Assigned to</label>
                     <select name="assignment" id="taskAssignment" required>
                         <option value="" disabled selected>Select contacts to assign</option>
@@ -43,11 +43,11 @@ function HTMLrenderPopUpAddTask() {
             </div>
             <div id="addTaskOverlayBodyMiddle"></div>
             <div id="addTaskOverlayBodyRight">
-                <div id="elementInAddTaskOverlayBody">
+                <div class="elementInAddTaskOverlayBody">
                     <label>Due Date</label>
-                    <input type="date" placeholder="dd/mm/yyyy" required>
+                    <input id="taskDate" class="taskInput" type="date" placeholder="dd/mm/yyyy" required>
                 </div>
-                <div id="elementInAddTaskOverlayBody">
+                <div class="elementInAddTaskOverlayBody">
                     <label>Prio</label>
                     <div class="addtask-prio-main-container">
                         <div class="addtask-prio-container" id="prio-urgent" onclick="prioContainer('urgent')"
@@ -64,30 +64,59 @@ function HTMLrenderPopUpAddTask() {
                         </div>
                     </div>
                 </div>
-                <div id="elementInAddTaskOverlayBody">
+                <div class="elementInAddTaskOverlayBody">
                     <label>Subtasks</label>
-                    <div class="subTask">
-                        <input class="test" type="text" placeholder="Add new subtask">
-                        <div class="subTaskImage">
+                    <div class="newSubTaskInputArea">
+                        <input class="taskInput" id="addNewSubtask" type="text" placeholder="Add new subtask">
+                        <div class="subTaskImage" onclick="renderNewSubtask()">
                             <img src="./../img/add-subtask.svg">
                         </div>
                     </div>
+                </div>
+                <div class="elementInAddTaskOverlayBody" id="subTaskList">
+
                 </div>
             </div>
         </div>
         <div class="addTaskOverlayBottom">
             <div class="add-task-bottom-button-container">
-                <button class="button-clear" onmouseover="buttonCreateTaskChangeColor()"
-                    onmouseout="buttonCreateTaskChangeColorBack()">Clear<svg width="25" height="24" viewBox="0 0 25 24"
+                <button type="reset" class="button-clear" onmouseover="buttonCreateTaskChangeColor()"
+                    onmouseout="buttonCreateTaskChangeColorBack()" onclick="clearAddTaskFields()">Clear<svg width="25" height="24" viewBox="0 0 25 24"
                         fill="none" xmlns="http://www.w3.org/2000/svg" id="add-task-icon-cancel">
                         <path
                             d="M12.2495 12.0001L17.4925 17.2431M7.00653 17.2431L12.2495 12.0001L7.00653 17.2431ZM17.4925 6.75708L12.2485 12.0001L17.4925 6.75708ZM12.2485 12.0001L7.00653 6.75708L12.2485 12.0001Z"
                             stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                     </svg>
                 </button>
-                <button class="button-create-task">Create Task<img src="./../img/check.svg"
+                <button type="submit" class="button-create-task">Create Task<img src="./../img/check.svg"
                         id="button-create-task"></button>
             </div>
     </form>
     `;
+}
+
+function renderNewSubtask() {
+    let newSubTaskValue = document.getElementById('addNewSubtask').value;
+    let subTaskList = document.getElementById('subTaskList');
+    subTaskList.innerHTML += HTMLrenderNewSubtask(newSubTaskValue);
+}
+
+function HTMLrenderNewSubtask(newSubTaskValue) {
+    return /*html*/`
+        <div class="subTask">
+            <input type="checkbox"><span>${newSubTaskValue}</span>
+        </div>
+    `;
+}
+
+function clearAddTaskFields(){
+    let addTaskElements = document.getElementsByClassName('taskInput');
+    for (let index = 0; index < addTaskElements.length; index++) {
+        let elementByID = addTaskElements[index]['id'];
+        let setPlaceholder = document.getElementById(`${elementByID}`).placeholder;
+        let emptyValue = document.getElementById(`${elementByID}`);
+        emptyValue.value = '';
+        document.getElementById(`${elementByID}`).placeholder = `${setPlaceholder}`;
+    }
+    document.getElementById('subTaskList').innerHTML = '';
 }
