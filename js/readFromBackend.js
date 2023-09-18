30c59d4c94d523e49b0f5816ae41d2d0db63b30e
Andreas Krapalies <akrapalies@gmail.com>
2023-09-18 08:15:39 +0200

    Worked on login and backend registration

---
+++ js/readFromBackend.js
@@ -0,0 +1,36 @@
+async function initUsers(){
+    loadUsers();
+}
+
+async function loadUsers(){
+    try {
+        usersjoin = JSON.parse(await getItem('usersjoin'));
+    } catch(e){
+        console.error('Loading error:', e);
+    }
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json()).then(res => {
+        if (res.data) { 
+            return res.data.value;
+        } throw `Could not find data with key "${key}".`;
+    });
+}
+
+function loginCheck(){
+    let loginTryUsername = document.getElementById('loginMail').value;
+    let loginTryPassword = document.getElementById('loginPasswort').value;
+
+    for (let index = 0; index < usersjoin.length; index++) {
+        if(usersjoin[index]['email'].includes(loginTryUsername) & usersjoin[index]['password'].includes(loginTryPassword)){
+            alert('Der User ist vorhanden!');
+            return;
+        }
+        else{
+            alert('Der User ist nicht vorhanden!');
+            return;
+        }
+    }
+}
--- js/register.js
+++ js/register.js
@@ -1,4 +1,69 @@
-let users = [{ email: "test@gmail.com", password: "test123" }];
+let usersjoin = [];
+let registerName = document.getElementById('userName');
+let registerEmail = document.getElementById('email');
+let registerPassword = document.getElementById('password');
+let checkboxAlert = document.getElementById("checkboxAlert");
+
+function toggleConfirmPassword() {
+  let signUpBtn = document.getElementById('signUpButton');
+  signUpBtn.disabled = true;
+
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("confirmPassword");
+
+  if (password === confirmPassword.value) {
+    confirmPassword.style.border = "1px solid #ccc";
+    document.getElementById("passwordMatchMessage").style.display = "none";
+    signUpBtn.disabled = false;
+  } else {
+    confirmPassword.style.border = "1px solid red";
+    document.getElementById("passwordMatchMessage").style.display = "block";
+  }
+}
+
+async function registerUser() {
+  if (!checkbox.checked) {
+    checkboxAlert.style.display = "block";
+    return;
+  }
+
+  checkboxAlert.style.display = "none";
+  checkUser();
+}
+
+async function checkUser() {
+  // await loadUsers();
+  debugger;
+  if (usersjoin.length === 0) {
+    fillLocalArray();
+  }
+  if (usersjoin.length !== 0) {
+    for (let index = 0; index < usersjoin.length; index++) {
+      const element = usersjoin[index];
+      const boolregisterEmail = element['usermail'].includes(registerEmail.value);
+      if (boolregisterEmail) {
+        alert('User existiert bereits');
+        return;
+      }
+      else if (!boolregisterEmail) {
+        fillLocalArray();
+        await setItem('usersjoin', JSON.stringify(usersjoin));
+        alert('User wurde angelegt');
+        window.location.href = "login.html";
+        return;
+      }
+    }
+  }
+}
+
+function fillLocalArray() {
+  usersjoin.push({
+    username: registerName.value,
+    usermail: registerEmail.value,
+    userpassword: registerPassword.value
+  });
+}
+
 
 function addUser() {
   let email = document.getElementById("email");
@@ -13,21 +78,8 @@ function addUser() {
 
   checkboxAlert.style.display = "none";
 
-  users.push({ email: email.value, password: password.value });
+  usersjoin.push({ email: email.value, password: password.value });
   window.location.href = "login.html";
-}
-
-function toggleConfirmPassword() {
-  let password = document.getElementById("password").value;
-  let confirmPassword = document.getElementById("confirmPassword");
-
-  if (password === confirmPassword.value) {
-    confirmPassword.style.border = "1px solid #ccc";
-    document.getElementById("passwordMatchMessage").style.display = "none";
-  } else {
-    confirmPassword.style.border = "1px solid red";
-    document.getElementById("passwordMatchMessage").style.display = "block";
-  }
 }
 
 function showPassword() {
@@ -55,7 +107,3 @@ function showConfirmPassword() {
     lockIcon.src = "assets/img/hide.png";
   }
 }
-
-
-
-
