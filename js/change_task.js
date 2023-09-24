fc9ba19e206a10c5054c5c55ff601f9183cc32c5
Andreas Krapalies <akrapalies@gmail.com>
2023-09-24 17:47:05 +0200

    Wokred on board

--- js/change_task.js
+++ js/change_task.js
@@ -3,11 +3,12 @@ function renderPopUpChangeTask(id) {
     popUp.innerHTML = HTMLrenderChangeTask(id);
     openAddTaskForm();
 }
+
 function HTMLrenderChangeTask(id){
     return /*html*/ `
-    <div id="${id}" class="existingTaskOverviewPopUp">
-    <div class="taskForm">
-        <div class="taskFormHeader">
+<div id="${id}" class="existingTaskOverviewPopUp">
+    <div class="taskFormHeader">
+        <div class="taskFormFirstHeader">
             <span class="bgc-technicaltask taskHeadline-bg">${todos[id]['taskCategory']}</span>
             <img class="cross-close-editForm" onclick="closeAddTaskForm()" src="./../img/cross.png">
         </div>
@@ -16,18 +17,32 @@ function HTMLrenderChangeTask(id){
             <span class="taskFormSecondHeader-description">${todos[id]['taskInputDescription']}</span>
         </div>
     </div>
-    <div class="editForm">
-        <div id="dateInfo">
-            <div>Due Date</div>
-            <div>Date of Array</div>
+    <div class="taskFormBody">
+        <div class="priorityAndDateInfo">
+            <p>Due date: </p>
+            <p>Priority</p>
         </div>
-        <div id="priorityInfo">
-            <div>Priority</div>
-            <div>urgent mit Zeichen und button style</div>
+        <div class="priorityAndDateInfo">
+            <p>${todos[id]['taskInputDate']}</p>
+            <p>${todos[id]['taskPriority']}</p>
         </div>
-        <div id="infoAssignedTo">
-            <div>Asigned to:</div>
-            <div>Hier kommen dann die Divs mit Namen</div>
+    </div>
+    <div id="infoAssignedTo">
+        <p>Asigned to:</p>
+        <div>Hier kommen dann die Divs mit Namen</div>
+    </div>
+    <div id="subtasks">
+        <p>Subtasks</p>
+        <div>Hier kommen dann die Divs mit Namen</div>
+    </div>
+    <div class="deleteEditContainer">
+        <div class="deleteEditFunctions" onclick="deleteTask(${id})">
+            <img src="./../img/delete.svg">
+            <span>Delete</span>
+        </div>
+        <div class="deleteEditFunctions">
+            <img src="./../img/edit.svg">
+            <span>Edit</span>
         </div>
     </div>
 </div>
