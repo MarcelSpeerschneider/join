88157370df5c7a78fb33f242e93488d29a905690
Andreas Krapalies <akrapalies@gmail.com>
2023-09-26 14:47:13 +0200

    Worked on Board - Edit Tasks

--- js/change_task.js
+++ js/change_task.js
@@ -2,6 +2,8 @@ function renderPopUpChangeTask(id) {
     let popUp = document.getElementById('overlay');
     popUp.innerHTML = HTMLrenderChangeTask(id);
     inputSubTasksHTML(id);
+    inputAssignedToHTML(id);
+    setPrio(id);
     openAddTaskForm();
 }
 
@@ -21,20 +23,24 @@ function HTMLrenderChangeTask(id) {
     <div class="taskFormBody">
         <div class="priorityAndDateInfo">
             <p>Due date: </p>
-            <p>Priority</p>
+            <p>Priority:</p>
         </div>
         <div class="priorityAndDateInfo">
             <p>${todos[id]['taskInputDate']}</p>
-            <p>${todos[id]['taskPriority']}</p>
+            <div id="taskPriority">
+
+            </div>
         </div>
     </div>
-    <div id="infoAssignedTo">
+    <div>
         <p>Asigned to:</p>
-        <div>Hier kommen dann die Divs mit Namen</div>
+        <div id="assignedToList" class="subTaskList-and-assignedToList">
+
+        </div>
     </div>
     <div id="subtasksOverview">
         <p>Subtasks:</p>
-        <div id="subtasks" class="subtasks">
+        <div id="subTaskList" class="subTaskList-and-assignedToList">
 
         </div>
     </div>
@@ -55,8 +61,8 @@ function HTMLrenderChangeTask(id) {
 function inputSubTasksHTML(id) {
     let subTasks = todos[id]['taskSubtasks'];
     for (let index = 0; index < subTasks.length; index++) {
-        document.getElementById('subtasks').innerHTML += /*html*/ `
-            <div class="subtask">
+        document.getElementById('subTaskList').innerHTML += /*html*/ `
+            <div class="subTask-and-assignedTo">
                 <input type="checkbox" id="[${id}][${index}]" onclick="subTaskChecked(this.id)">
                 <label>${subTasks[index]}</label>
             </div>
@@ -64,6 +70,27 @@ function inputSubTasksHTML(id) {
     }
 }
 
+function inputAssignedToHTML(id) {
+    let assignedTo = todos[id]['tasksAssignedTo'];
+    for (let index = 0; index < assignedTo.length; index++) {
+        document.getElementById('assignedToList').innerHTML += /*html*/ `
+            <div class="subTask-and-assignedTo">
+                <label>Acronym</label>
+                <label>${assignedTo[index]}</label>
+            </div>
+            `;
+    }
+}
+
 function subTaskChecked(id){
     document.getElementById(`${id}`).checked = true;
+}
+
+function setPrio(id){
+    let adjustedPrioName = todos[id]['taskPriority'].slice(5)
+    adjustedPrioName = adjustedPrioName.charAt(0).toUpperCase() + adjustedPrioName.slice(1);
+    document.getElementById('taskPriority').innerHTML += /*html*/ `
+        <div>${adjustedPrioName}</div>
+        <img src="./assets/img/pri-low.svg">
+    `;
 }
